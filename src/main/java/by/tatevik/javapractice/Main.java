dd59e223a0bc2f59f04dc393f46cc57be9038377
Tatevik777 <67191920+Tatevik777@users.noreply.github.com>
2024-12-09 00:19:42 +0300

    update

--- src/main/java/by/tatevik/javapractice/Main.java
+++ src/main/java/by/tatevik/javapractice/Main.java
@@ -1,11 +1,33 @@
 package by.tatevik.javapractice;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         task1();
-
+        int[] arr=new int[27];
+        task2(arr);
+        System.out.println(Arrays.toString(arr));
     }
     public static void task1(){
+
+
+    }
+    public static void task2(int[]arr){
+        //заполнить массив  первыми 25 натуральными числами(1,2,3,4.....)
+        //затем добавить в него 100, 200
+        int index=0;
+        int num=1;
+        while(index<=24){
+            arr[index]=num;
+            index=index+1;
+            num=num+1;
+        }
+        arr[25]=100;
+        arr[26]=200;
+    }
+
+    public static void task(){
         //Создать метод public static void task1().
         //Внутри него объявить 2 переменных типа int,
         // проинициализировать их прямо в коде. Затем
@@ -19,6 +41,5 @@ public class Main {
         num2=container;
 
     }
-
 
 }
