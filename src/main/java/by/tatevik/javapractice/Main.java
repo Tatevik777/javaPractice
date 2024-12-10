6a8ab790de59c381a08e4d063dce8c34568034e4
Tatevik777 <67191920+Tatevik777@users.noreply.github.com>
2024-12-10 22:09:46 +0300

    update

--- src/main/java/by/tatevik/javapractice/Main.java
+++ src/main/java/by/tatevik/javapractice/Main.java
@@ -4,18 +4,23 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        task1();
-        int[] arr=new int[27];
-        task2(arr);
-        System.out.println(Arrays.toString(arr));
+        int[] arr2=new int[]{11,12,13,14,15,16,17,18};
+        printRevertArr(arr2);
+        //int[] arr=new int[27];
+       // task2(arr);
+       // System.out.println(Arrays.toString(arr));
     }
-    public static void task1(){
-
-
+    //вывести элементы массива в обратном порядке на экран
+    public static void printRevertArr(int[]arr){
+        int index=arr.length-1;
+        while(index>=0){
+            System.out.print(arr[index]+ " ");
+            index=index-1;
+        }
     }
+    //заполнить массив  первыми 25 натуральными числами(1,2,3,4.....)
+    //затем добавить в него 100, 200
     public static void task2(int[]arr){
-        //заполнить массив  первыми 25 натуральными числами(1,2,3,4.....)
-        //затем добавить в него 100, 200
         int index=0;
         int num=1;
         while(index<=24){
@@ -26,20 +31,18 @@ public class Main {
         arr[25]=100;
         arr[26]=200;
     }
-
+    //Создать метод public static void task1().
+    //Внутри него объявить 2 переменных типа int,
+    // проинициализировать их прямо в коде. Затем
+    // поменять местами значения в переменных без использования
+    // литералов, но с использованием дополнительной переменной.
     public static void task(){
-        //Создать метод public static void task1().
-        //Внутри него объявить 2 переменных типа int,
-        // проинициализировать их прямо в коде. Затем
-        // поменять местами значения в переменных без использования
-        // литералов, но с использованием дополнительной переменной.
         int num1=20;
         int num2=25;
         int container;
         container=num1;
         num1=num2;
         num2=container;
-
     }
 
 }
